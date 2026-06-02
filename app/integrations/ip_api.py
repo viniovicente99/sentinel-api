834da46591d6316bbd51c2efa1f0eea7994cdb72
viniovicente99 <viniovicente99@gmail.com>
2026-06-02 20:57:42 -0300

    Adicionada a lógica de Retry para a API de Geolocalização

--- app/integrations/ip_api.py
+++ app/integrations/ip_api.py
@@ -1,15 +1,34 @@
 import requests
-
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
 
 load_dotenv()
 
 IP_API = os.getenv("IP_API_URL")
 
-def get_geo_location(ip: str):
+session = requests.Session()
 
-    response = requests.get(f"{IP_API}/{ip}")
+retry = Retry(
+    total=3,
+    backoff_factor=1,
+    status_forcelist=[429, 500, 502, 503, 504],
+    allowed_methods=["GET"],
+)
+
+adapter = HTTPAdapter(max_retries=retry)
+
+session.mount("http://", adapter)
+session.mount("https://", adapter)
+
+
+def get_geo_location(ip: str):
+    response = session.get(
+        f"{IP_API}/{ip}",
+        timeout=5,
+    )
 
     response.raise_for_status()
 
